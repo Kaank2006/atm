a8d3973e049cb7c669954a4ad0baff4e01374d1b
earthpwn <emreartuc@gmail.com>
2020-12-27 18:09:26 +0300

    Latest code from Ege's local

--- src/com/ders/Client.java
+++ src/com/ders/Client.java
@@ -3,17 +3,53 @@ package com.ders;
 import java.util.ArrayList;
 
 public class Client {
-    private int clientNumber;
+    private int clientNumber = 0;
     private String clientName;
-    private ArrayList<Account> clientAccounts = new ArrayList<Account>();
+    private String clientSurname;
+    private String clientCity;
+    private ArrayList<Account> clientAccounts = new ArrayList<>();
 
-    public ArrayList<Account> getAccounts(){
+    public ArrayList<Account> getAccounts() {
         return clientAccounts;
     }
 
-    public int addAccount(){
+    public int addAccount() {
         Account yeniHesap = new Account();
+        yeniHesap.setAccountNumber(clientAccounts.size() + 1);
         clientAccounts.add(yeniHesap);
         return yeniHesap.getAccountNumber();
+    }
+    public Client(String clientName, String clientSurname, String clientCity){
+        this.clientName = clientName;
+        this.clientSurname = clientSurname;
+        this.clientCity = clientCity;
+    }
+    public Client(){}
+    public int getClientNumber() {
+        return this.clientNumber;
+    }
+    public String getClientSurname() {
+        return this.clientSurname;
+    }
+    public String getClientCity() {
+        return this.clientCity;
+    }
+    public String getClientName() {
+        return this.clientName;
+    }
+    public void setClientName(String name){
+        clientName = name;
+    }
+    public void setClientNumber(int number) {
+        clientNumber = number;
+    }
+    public void setClientSurname(String surname) {
+        clientSurname = surname;
+    }
+    public void setClientCity(String city) {
+        clientCity = city;
+    }
+    public void setClientAccounts(ArrayList<Account> clientAccounts){
+        this.clientAccounts = clientAccounts;
     }
 }
